a38ea8a144afb58900d448db60f81d8c743339cc
Alan O'Connor <aocthatsme246@gmail.com>
2025-11-22 18:11:11 +0000

    Created Notifications model

--- backend/activity/models.py
+++ backend/activity/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from habits.models import Action
 
-class Notifications(models.Model):
+class Notification(models.Model):
     class NotificationCategoryChoice(models.TextChoices):
         USER_MISSED_HABIT = "user_missed_habit", "User Missed Habit"
         PARTNER_MISSED_HABIT = "partner_missed_habit", "Partner Missed Habit"
         STREAK_MILESTONE = "streak_milestone", "Streak Milestone"
+        ROUTINE_COMPLETED = "routine_completed", "Routine Completed"
         PARTNERSHIP_REQUEST = "partnership_request", "Partnership Request"
         PARTNERSHIP_REQUEST_ACCEPTED = "partnership_request_accepted", "Partnership Request Accepted"
         PARTNERSHIP_REQUEST_REJECTED = "partnership_request_rejected", "Partnership Request Rejected"
@@ -20,7 +21,9 @@ class Notifications(models.Model):
     created_by = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
-        related_name="created_notifications"
+        related_name="created_notifications",
+        null=True,
+        blank=True
     )
 
     message = models.TextField(
@@ -30,7 +33,8 @@ class Notifications(models.Model):
 
     category = models.CharField(
         verbose_name="Category",
-        choices=NotificationCategoryChoice.choices
+        choices=NotificationCategoryChoice.choices,
+        max_length=50
     )
 
     read_status = models.BooleanField(
@@ -55,3 +59,27 @@ class Notifications(models.Model):
         verbose_name = "Notification"
         verbose_name_plural = "Notifications"
         ordering = ["-created_at"]
+
+        constraints = [
+            models.CheckConstraint(
+                name="habit_trigger_is_action_and_action_exists",
+                check=
+                    ~models.Q(category__in=["user_missed_habit", "partner_missed_habit", "streak_milestone"]) |
+                    models.Q(trigger_action__isnull=False)
+            ),
+            models.CheckConstraint(
+                name="partnership_request_does_not_have_trigger_action",
+                check=
+                    ~models.Q(category__startswith="partnership") |
+                    models.Q(trigger_action__isnull=True)
+            ),
+            models.CheckConstraint(
+                name="partnership_request_has_a_created_by_user",
+                check=
+                    ~models.Q(category="partnership") |
+                    models.Q(created_by__isnull=False)
+            )
+        ]
+    
+    def __str__(self):
+        return f"{self.category} for {self.recipient.username} at {self.created_at}"
